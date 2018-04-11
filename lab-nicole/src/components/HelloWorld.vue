89d8eb7a1e8564ba4c4d02cfb9a04ca99d76ee27
nicoleweese <nweese@ymail.com>
2018-04-11 16:56:50 -0700

    added peace out button

---
+++ lab-nicole/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+    {{ question }}
+    <p>Who am I speaking with?:</p>
+    </p> {{ name }}</p>
+    <input v-model="name" placeholder="name">
+    <p>How many pets would you like to own?</p>
+    {{ pets }} <button v-on:click="pets -=1">-</button> <button v-on:click="pets += 1">+</button>
+    <br><br>
+    <button v-on:click="peace">I'm done with you!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Hello World!',
+      name: '',
+      pets: 0,
+    }
+  },
+  methods: {
+    peace: function() {
+      alert('Sad to see you go!');
+    }
+  }
+}
+</script>
